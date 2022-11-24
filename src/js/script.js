d3b5a991100510b6c4b390d2c188f1cca397f444
Anton <abodyk1516@gmail.com>
2022-11-24 15:29:03 +0200

    third commit

--- src/js/script.js
+++ src/js/script.js
@@ -32,4 +32,82 @@ $(document).ready(function(){
     };
     toggleSlide('.catalog-item-link');
     toggleSlide('.catalog-item-back');
+
+    //----------------Modals 
+    $('[data-modal="consultation"]').on('click', function(){
+        $('.overlay, #consultation').fadeIn();
+    })
+    $('.modal-close').on('click', function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut();
+    })
+    $('.button-mini').on('click', function(){
+        $('.overlay, #order').fadeIn();
+    })
+    $('.button-mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal-descr').text($('.catalog-item-subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+        })
+    })
+
+ function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свой почтовый адрес",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $("input[name=phone]").mask("+38(099) 999-9999");
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        
+        if(!$(this).valid()){
+            return;
+        }
+        $.ajax({
+            type: "POST",
+    //         url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn();
+            $('form').trigger('reset');
+        });
+        return false;
+    })
+
+    //---------------PAGE-UP 
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600){
+            $('.pageup').fadeIn();
+        }else{
+            $('.pageup').fadeOut();
+        }
+    })
   });
